57ef035b42cbdb7b19d4b6cc52ea8885ac810271
jaensen <daniel@circles.name>
2022-08-25 03:25:43 +0200

    loopback received events in the MockStatefulEndpoint

--- common/channels/tests/mocks/MockStatefulEndpoint.ts
+++ common/channels/tests/mocks/MockStatefulEndpoint.ts
@@ -3,6 +3,7 @@ import {IEndpoint} from "@circlesland/interfaces-channels/src/IEndpoint";
 import {IUniqueEvent} from "@circlesland/interfaces-channels/src/IUniqueEvent";
 import {MockEndpoint} from "./MockEndpoint";
 import {StatefulEndpoint} from "../../src/StatefulEndpoint";
+import {MockRequest} from "./MockRequest";
 
 export class MockStatefulEndpoint implements IStatefulEndpoint {
   readonly endpoint: IEndpoint;
@@ -11,6 +12,10 @@ export class MockStatefulEndpoint implements IStatefulEndpoint {
   constructor() {
     this.endpoint = MockEndpoint.instance;
     this.wrappedStatefulEndpoint = new StatefulEndpoint(this.endpoint, 1000);
+
+    // Register a loopback sink
+    this.wrappedStatefulEndpoint.endpoint.sink.receive(MockRequest.type,
+      (event) => this.wrappedStatefulEndpoint.endpoint.source.emit(event));
   }
 
   request(request: IUniqueEvent, timeoutIn?: number): Promise<IUniqueEvent> {

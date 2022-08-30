23d2add8c868cf25a2f2ac1a5aa716cfb2f69f58
andreirogobete <rogobete.andrei@gmail.com>
2022-08-30 13:14:48 +0300

    Add interfaces for frame interaction events

---
+++ packages/ui-components/src/types/view.ts
@@ -0,0 +1,18 @@
+/**
+ * An interface that defines currently supported view types
+ */
+export enum ViewType {
+    HORIZONTAL_LAYOUT,
+    VERTICAL_LAYOUT
+}
+
+/**
+ * Basic view definition for a UI Component. All UI Components should derive from this interface
+ */
+export interface View {
+    id: string;
+    testId: string;
+    type: ViewType;
+    args: { [key: string]: any };
+    children?: View[]
+}

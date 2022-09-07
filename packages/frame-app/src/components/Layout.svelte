474a920e7d3299cde6edff2201843372fc639b98
Razvan Statescu <razvan@statescu.net>
2022-09-07 11:16:49 +0300

    add theme toggle

--- packages/frame-app/src/components/Layout.svelte
+++ packages/frame-app/src/components/Layout.svelte
@@ -1,3 +1,16 @@
+<script lang="ts">
+  import { ThemeHelper, Themes } from './../../utils/ThemeHelper';
+  const changeTheme = (theme: Themes) => {
+    ThemeHelper.changeTheme(theme);
+  };
+</script>
+
 <div class="mt-16">
+  <div>
+    <button on:click={() => changeTheme(Themes.default)}
+      >Change to default</button
+    >
+    <button on:click={() => changeTheme(Themes.dark)}>Change to dark</button>
+  </div>
   <slot />
 </div>

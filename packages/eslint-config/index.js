46e22a10daa78892b85638f30bb00abf9e92b893
Razvan Statescu <razvan@statescu.net>
2022-09-23 19:59:11 +0300

    create eslint package

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/rule-name': 'error',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  ignorePatterns: ['dist/*'],
+};

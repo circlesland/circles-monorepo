2c8d333c19f3e54c3eba6a59af1a989e286ce112
Andrei Rogobete <andreirogobete@Andreis-MacBook-Pro.local>
2022-11-03 14:40:28 +0200

    add new test

--- packages/ui-components/cypress/tests/button.cy.ts
+++ packages/ui-components/cypress/tests/button.cy.ts
@@ -6,7 +6,7 @@ describe('Button', () => {
   const buttonSelector = '[data-testId="button-test-id"]';
   it('should display a button with an icon', async () => {
     let clickCount = 0;
-    const clickCallback = function () {
+    const clickCallback = () => {
       clickCount++;
     };
     const view: View & ButtonType = {
@@ -28,11 +28,36 @@ describe('Button', () => {
     };
 
     cy.mount(Button, { props: { view } });
-
     cy.get(buttonSelector).click();
 
     await waitForAsync(() => clickCount === 1);
+    expect(clickCount).to.eq(1);
+  });
 
+  it('should display a circular icon button', async () => {
+    let clickCount = 0;
+    const clickCallback = () => {
+      clickCount++;
+    };
+
+    const view: View & ButtonType = {
+      id: 'button',
+      callback: clickCallback,
+      type: ViewType.BUTTON,
+      testId: 'button-test-id',
+      args: {
+        type: ButtonTypes.CircularWithIcon,
+        icon: {
+          source: 'academic-cap',
+          solid: true,
+        },
+      },
+    };
+
+    cy.mount(Button, { props: { view } });
+    cy.get(buttonSelector).click();
+
+    await waitForAsync(() => clickCount === 1);
     expect(clickCount).to.eq(1);
   });
 });

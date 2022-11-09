8237a47501b07a3dd7ebf59cae5a070b47125a99
Andrei Rogobete <andreirogobete@Andreis-MacBook-Pro.local>
2022-11-09 12:42:08 +0200

    remove channels from ui-packages

--- packages/ui-components/src/components/VerticalLayout/VerticalLayout.svelte
+++ packages/ui-components/src/components/VerticalLayout/VerticalLayout.svelte
@@ -1,34 +1,11 @@
 <script lang="ts">
-  import type {
-    IChannel,
-    IDuplexChannel,
-  } from '@circlesland/interfaces-channels';
   import type { VerticalLayout } from '../../types';
-  import {
-    DuplexChannel,
-    PostMessageSink,
-    PostMessageSource,
-  } from '@circlesland/channels';
   import type { View } from '../../types';
   import { SupportedViews } from '../../types/supported';
 
   export let view: View & VerticalLayout;
 
   const classList = 'flex flex-col justify-around flex-wrap max-w-full';
-
-  const trigger = (trg: string) => {
-    const source = new PostMessageSource(window.parent, '*');
-    const sink = new PostMessageSink(window, window.location.origin);
-
-    const duplexChannel: IDuplexChannel = new DuplexChannel(source, sink);
-
-    duplexChannel.endpoint.send({
-      id: '2',
-      type: 'safeDappSdkMessage',
-      method: trg,
-      requestId: '1234',
-    });
-  };
 </script>
 
 {#if view && view.children}
@@ -38,7 +15,6 @@
         <svelte:component
           this={SupportedViews[child.type]}
           view={child}
-          on:click={() => trigger(child.trigger)}
         />
       {:else}
         <svelte:component this={SupportedViews[child.type]} view={child} />
